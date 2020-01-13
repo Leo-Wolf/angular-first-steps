14ef833f62b9ab3ac9ad39f0dff52759d343ce6e
Leonardo Barajas <leonardo.barajas@thomsonreuters.com>
2020-01-13 14:48:11 -0600

    Solving problemas

--- src/app/filter.pipe.ts
+++ src/app/filter.pipe.ts
@@ -6,7 +6,7 @@ export class FilterPipe implements PipeTransform {
   transform(value: any, arg: any): any {
     const resultPost = [];
     for ( const post of value ) {
-      if ( post.toLowerCase().includes(arg.toLowerCase() ) > -1 ) {
+      if ( post.name.toLowerCase().includes(arg.toLowerCase() ) > -1 ) {
         resultPost.push( post );
       }
     }
